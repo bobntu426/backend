8848363cc0a4e44afefd21cf42e769fa7bbd5870
bobntu426 <B08508021@ntu.edu.tw>
2022-02-06 05:10:43 +0800

    add some people property

--- server.js
+++ server.js
@@ -62,9 +62,21 @@ db.once('open', () => {
     console.log(`The server is up on port ${PORT}!`)
     if(people.length == 0){
       await People.insertMany([
-        {name:'張峻林',score:2000},
-        {name:'張家翔',score:1900},
-        {name:'張育杰',score:1800},
+        {name:'張峻林',score:2000,school:'台灣大學',id:1,popular:15,gender:'male'},
+        {name:'張家翔',score:1900,school:'台灣大學',id:2,popular:15,gender:'male'},
+        {name:'張育杰',score:1800,school:'輔仁大學',id:3,popular:15,gender:'male'},
+        {name:'陳烱濤',score:1700,school:'台灣大學',id:4,popular:15,gender:'male'},
+        {name:'任家良',score:1600,school:'慈濟大學',id:5,popular:15,gender:'male'},
+        {name:'林恭緯',score:1500,school:'聯合大學',id:6,popular:15,gender:'male'},
+        {name:'陳乃深',score:1400,school:'中原大學',id:7,popular:15,gender:'male'},
+        {name:'李柏諺',score:1300,school:'台灣大學',id:8,popular:15,gender:'male'},
+        {name:'葉昇翰',score:1200,school:'高雄科技大學',id:9,popular:15,gender:'male'},
+        {name:'張友柏',score:1100,school:'逢甲大學',id:10,popular:15,gender:'male'},
+        {name:'張霈承',score:1000,school:'中央大學',id:11,popular:15,gender:'male'},
+        {name:'彭偉豪',score:900,school:'台灣師範大學',id:12,popular:15,gender:'male'},
+        {name:'陳睿恩',score:800,school:'台灣大學',id:13,popular:15,gender:'male'},
+        {name:'劉又嘉',score:700,school:'台灣大學',id:14,popular:15,gender:'male'},
+        {name:'許兆鈞',score:600,school:'台灣大學',id:15,popular:15,gender:'male'}
       ])
       console.log('init data')
     }
